94815c22269739202ed62db5c8e617daf2deb6e4
nkanyi73 <nkanyi73@gmail.com>
2024-04-15 12:56:48 +0100

    Fix bug with interactables and add material on wire dip into liquid

--- Assets/Scripts/WireTagManager.cs
+++ Assets/Scripts/WireTagManager.cs
@@ -14,6 +14,17 @@ public class WireTagManager : MonoBehaviour
     {
         if (other.gameObject.layer == 6)
         {
+            if (gameObject.tag == "CopperChloride")
+            {
+                Transform liquidMaterial = other.gameObject.transform.Find("Liquid Material");
+                liquidMaterial.GetComponent<MeshRenderer>().enabled = true;
+                //liquidMaterial.GetComponent<MeshRenderer>().material = blueLiquidMaterial;
+            } else
+            {
+                Transform liquidMaterial = other.gameObject.transform.Find("White Material");
+                //liquidMaterial.GetComponent<MeshRenderer>().SetMaterials(whiteLiquidMaterial);
+                liquidMaterial.GetComponent<MeshRenderer>().enabled = true;
+            }
             other.gameObject.tag = gameObject.tag.ToString();
         }
     }
