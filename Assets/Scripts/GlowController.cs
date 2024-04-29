348937b7ebd90fd14da05d96d56c7214e29f5c3e
nkanyi73 <nkanyi73@gmail.com>
2024-04-29 11:31:51 +0100

    Add flashing animation for Nichrome Wire and start on glow controller script

---
+++ Assets/Scripts/GlowController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class GlowController : MonoBehaviour
+{
+    public Material metalMaterial;
+    public Color glowColor = Color.red;
+    public float maxGlowIntensity = 2.0f;
+    public float glowSpeed = 1.0f;
+
+    public Color originalColor;
+    private float currentGlowIntensity = 0.0f;
+    private bool isNearFire = false;
+
+    void Start()
+    {
+        // Store original material color
+        originalColor = metalMaterial.GetColor("_EmissionColor");
+    }
+
+    void Update()
+    {
+        if (isNearFire)
+        {
+            // Increase glow intensity
+            currentGlowIntensity = Mathf.Lerp(currentGlowIntensity, maxGlowIntensity, Time.deltaTime * glowSpeed);
+            metalMaterial.SetColor("_EmissionColor", glowColor * currentGlowIntensity);
+        }
+        else
+        {
+            // Decrease glow intensity
+            currentGlowIntensity = Mathf.Lerp(currentGlowIntensity, 0.0f, Time.deltaTime * glowSpeed);
+            metalMaterial.SetColor("_EmissionColor", originalColor * currentGlowIntensity);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Flame"))
+        {
+            Debug.LogError("Collision Detected");
+            isNearFire = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Flame"))
+        {
+            isNearFire = false;
+        }
+    }
+}
